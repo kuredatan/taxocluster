2916a5f9ba0218015b79ef1a4509c37c4166a751
Clémence <reda.clmc@live.fr>
2016-07-19 10:23:33 +0200

    sorting + unique

--- script_format_per_file.sh
+++ script_format_per_file.sh
@@ -4,7 +4,7 @@ echo $1;
 cut -d ' ' -f 2- $1 > ./meta/match/testfiles/file2$2.tmp;
 grep -v '*' ./meta/match/testfiles/file2$2.tmp > ./meta/match/testfiles/file1$2.tmp;
 grep -v ':' ./meta/match/testfiles/file1$2.tmp > ./meta/match/testfiles/file3$2.tmp;
-awk -F ' ' '{print NF > "./meta/match/testfiles/numbers.tmp"}' ./meta/match/testfiles/file2$2.tmp;
+awk -F ' ' '{!seen[$0]++ | print NF > "./meta/match/testfiles/numbers.tmp"}' ./meta/match/testfiles/file2$2.tmp;
 awk '{s+=$1} END {print s > "./meta/match/testfiles/number.tmp"}' ./meta/match/testfiles/numbers.tmp;
 cat ./meta/match/testfiles/number.tmp ./meta/match/testfiles/file2$2.tmp > ./meta/match/testfiles/file$2.test;
 rm -f ./meta/match/testfiles/*.tmp;
